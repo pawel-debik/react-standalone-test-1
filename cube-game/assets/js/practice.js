3b9ed2fd9ed6906703d9cdc6737e26f34ac7230a
Pawel <paweldebik@gmail.com>
2023-01-04 18:46:30 +0100

    added sounds, removed, skull, added green block, bugfixes

--- cube-game/assets/js/practice.js
+++ cube-game/assets/js/practice.js
@@ -3,34 +3,48 @@ $( document ).ready(function() {
     let gameScore = 0;
 
     $( ".squares" ).on( "click", ".square", function() {
-        if ( $(this).hasClass('active') ) {
-            // reset active squares
-            $('.square').removeClass('active');
-            $('.square').removeClass('danger');
 
+        if ( $(this).hasClass('active') ) {
+            resetSquares();
+            console.log('active go reset' );
             let randomSquare = Math.floor((Math.random() * squares) + 1);
             const clickSound = document.createElement('audio');
             clickSound.setAttribute('src', '../cube-game/assets/sounds/hit.mp3');
-
-            gameScore++;
-            updateSquares(randomSquare);
-            updateGamescore();
             clickSound.play();
+            gameScore++;
+            updateSquares();
+            updateGamescore();
         }
 
         if ( $(this).hasClass('danger') ) {
+            resetSquares();
+            console.log('danger go reset' );
             const clickSound = document.createElement('audio');
-            clickSound.setAttribute('src', '../cube-game/assets/sounds/hit.mp3');
-
-            $(this).hide();
-
+            clickSound.setAttribute('src', '../cube-game/assets/sounds/danger.mp3');
+            clickSound.play();
             gameScore = gameScore -5;
             updateSquares();
             updateGamescore();
-            clickSound.play();
         }
 
+        if ( $(this).hasClass('bonus') ) {
+            resetSquares();
+            console.log('bonus go reset' );
+            const clickSound = document.createElement('audio');
+            clickSound.setAttribute('src', '../cube-game/assets/sounds/bonus.mp3');
+            clickSound.play();
+            gameScore = gameScore +3;
+            updateSquares();
+            updateGamescore();
+        }
     });
+
+    function resetSquares() {
+        console.log('resetting' );
+        $('.square').removeClass('active');
+        $('.square').removeClass('danger');
+        $('.square').removeClass('bonus');
+    }
 
     function updateSquares() {
         let randomSquare = Math.floor((Math.random() * squares) + 1);
@@ -38,7 +52,9 @@ $( document ).ready(function() {
         $('.square').each(function() {
     
             // Each round set one active cube
-            if ( i == randomSquare ) {
+            console.log('i', i);
+            console.log('randomSquare', randomSquare);
+            if ( i == randomSquare && !$(this).hasClass('danger') && !$(this).hasClass('bonus') ) {
                 $(this).addClass('active');
             }
     
@@ -46,6 +62,7 @@ $( document ).ready(function() {
         });
 
         insertDangerSquare();
+        insertBonusSquare();
     }
 
     function insertDangerSquare() {
@@ -55,10 +72,28 @@ $( document ).ready(function() {
 
         $('.square').each(function() {
             // Some times set one danger cube
-            if ( i == randomSquare && !$(this).hasClass('active') ) {
+            if ( i == randomSquare && !$(this).hasClass('active') && !$(this).hasClass('bonus') ) {
                 console.log(sometimesDanger);
                 if ( sometimesDanger == 1 ) {
                     $(this).addClass('danger');
+                }
+            }
+
+            i++;
+        });
+    }
+
+    function insertBonusSquare() {
+        let randomSquare = Math.floor((Math.random() * squares) + 1);
+        let sometimesBonus = Math.floor(Math.random() * 10);
+        let i = 0;
+
+        $('.square').each(function() {
+            // Some times set one bonus cube
+            if ( i == randomSquare && !$(this).hasClass('active') && !$(this).hasClass('danger') ) {
+                console.log(sometimesBonus);
+                if ( sometimesBonus == 1 ) {
+                    $(this).addClass('bonus');
                 }
             }
 
